21c7fa0f3bb24cba0c1aa4219c6d9616453bf75a
Zelyna Farrell <zsillas@yahoo.com>
2023-11-13 17:06:48 -0800

    Initial Commit

---
+++ ScrollingTabView/ScrollingTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollingTabViewApp.swift
+//  ScrollingTabView
+//
+//  Created by Zelyna Sillas on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrollingTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
